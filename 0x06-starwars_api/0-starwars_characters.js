8488572d4b9a7f10c412c603bb97d8d083ddceee
kokole12 <kokoleismail886@gmail.com>
2023-08-11 17:59:56 +0300

    created call back function that fetches or requests for /people

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,6 +3,20 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+const getFromPeoople = (characters, index) => {
+  if (characters.length === index) {
+    return;
+  }
+
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      getFromPeoople(characters, index + 1);
+    }
+  });
+};
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
@@ -10,13 +24,14 @@ request.get(url, (error, response, body) => {
 
   const data = JSON.parse(body).characters;
 
-  for (const personUrl of data) {
-    request.get(personUrl, (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
-      const names = JSON.parse(body).name;
-      console.log(names);
-    });
-  }
+  // for (const personUrl of data) {
+  //   request.get(personUrl, (error, response, body) => {
+  //     if (error) {
+  //       console.log(error);
+  //     }
+  //     const names = JSON.parse(body).name;
+  //     console.log(names);
+  //   });
+  // }
+  getFromPeoople(data, 0);
 });
