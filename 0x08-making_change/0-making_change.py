fdc35a8cc865b94d4617f05f0d2e648b4a83b4f2
kokole12 <kokoleismail886@gmail.com>
2023-08-21 18:50:24 +0300

    changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""finding how many times coins add up to total"""
+
+
+def makeChange(coins, total):
+    """implementing the logic"""
+    if total <= 0:
+          return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+             break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
